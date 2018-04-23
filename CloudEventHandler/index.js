1fa06fa5e0453dfe9358f8c21e34d228fd0081c7
clemensv <clemensv@microsoft.com>
2018-04-23 14:33:51 +0200

    changed plain hook

--- CloudEventHandler/index.js
+++ CloudEventHandler/index.js
@@ -1,7 +1,7 @@
-module.exports = function (context, data) {
+module.exports = function (context, request) {
     context.log('Webhook was triggered!');
 
     // Check if we got first/last properties
-    context.log("data: " + data);
+    context.log("data: " + request.rawBody);
     context.done();
 }
