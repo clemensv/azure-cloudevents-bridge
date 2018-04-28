3ad3aa3dedd688c949e2eac0e696a119ca4a8ff3
clemensv <clemensv@microsoft.com>
2018-04-28 10:14:33 +0200

    better error handling

--- PickFlickrPicture/index.js
+++ PickFlickrPicture/index.js
@@ -8,28 +8,38 @@ module.exports = function (context, req) {
         user_id : process.env["FLICKR_USER_ID"], 
         photoset_id : process.env["FLICKR_PHOTOSET_ID"]
     }).then(function(res) {
-        var l = res.body.photoset.photo.length;
-        var p = Math.floor(Math.random() * Math.floor(l));
-        flickr.photos.getSizes({ photo_id : res.body.photoset.photo[p].id }).then(
-            function(resp){
-                var pic = resp.body.sizes.size.find(element => { 
-                    return element.width >= 1024; 
-                }); 
-                context.log("Pic selected " + pic.source);
-                context.res = {
-                    // status: 200, /* Defaults to 200 */
-                    body: pic.source
-                };
-                context.done();
-            }).catch(function(err){
-                context.log("Error " + err);
+        context.log("Status " + res.status);
+        if ( res.status == 200 ) {
+            var l = res.body.photoset.photo.length;
+            var p = Math.floor(Math.random() * Math.floor(l));
+            flickr.photos.getSizes({ photo_id : res.body.photoset.photo[p].id }).then(
+                function(resp){
+                    if ( resp.status == 200 ) {
+                        var pic = resp.body.sizes.size.find(element => { 
+                            return element.width >= 1024; 
+                        }); 
+                        context.log("Pic selected " + pic.source);
+                        context.res = {
+                            // status: 200, /* Defaults to 200 */
+                            body: pic.source
+                        };
+                    } else {
+                        context.log("Couldn't fetch sizes: " + resp.status + " " + resp.rawBody);
+                    }
+                    context.done();
+                }).catch(function(err){
+                    context.log("Error " + err);
 
-                context.res = {
-                    status: 500
-                };
-                context.done();
-            });       
-        
+                    context.res = {
+                        status: 500
+                    };
+                    context.done();
+                });       
+       } else {
+            context.log("Couldn't fetch list: " + res.status + " " + res.rawBody);
+            context.done();
+       }
+       
     }).catch(function(err){ 
         context.log("Error " + err);
         context.res = {
